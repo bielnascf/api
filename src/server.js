12f2167605a41d93abee1bd183bdebcf2b0f30e9
Gabriel Nascimento <127701444+bielnascf@users.noreply.github.com>
2024-02-15 22:39:47 -0300

    ajustei path AppError

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("express-async-errors")
 
 const migrationsRun = require("./database/sqlite/migrations");
-const AppError = require("./utils/appError");
+const AppError = require("./utils/AppError");
 const express = require("express"); 
 const routes = require("./routes");
 const uploadConfig = require("./configs/upload");
